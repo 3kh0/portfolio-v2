2ce226cafe3ca72d8c8e89e0ddc2f3bd37b06f20
3kh0 <echo-the-coder@tuta.io>
2023-09-20 19:15:47 -0400

    fixes

--- scripts/adblockNotice.js
+++ scripts/adblockNotice.js
@@ -1,11 +1,22 @@
 // Idea source:
 // https://stefanbohacek.com/project/detect-missing-adblocker-wordpress-plugin/
 
+function shouldDisplayNotice() {
+  return localStorage.getItem('noticeClosed') !== 'true';
+}
+
+if (shouldDisplayNotice()) {
+  document.getElementById('ftf-dma-note').style.display = 'block';
+} else {
+  document.getElementById('ftf-dma-note').style.display = 'none';
+}
+
+// close notice and store
 function closeNotice() {
   var notice = document.getElementById('ftf-dma-note');
-  notice.style.animation = 'slide-down 0.5s ease-out'; // Add slide-up animation
+  notice.style.animation = 'slide-down 0.5s ease-out'; // slides out of dms
   setTimeout(function () {
     notice.style.display = 'none';
-  }, 500); // Hide notice after animation completes (300ms)
+    localStorage.setItem('noticeClosed', 'true'); // store it
+  }, 500);
 }
-// TODO: store this to localstorage so it doesn't show again if the user clicks on close
