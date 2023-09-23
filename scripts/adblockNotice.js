af75a74d05edefbf3dd9d2a0dc0ad6e97d4254f2
3kh0 <echo-the-coder@tuta.io>
2023-09-22 21:48:43 -0400

    Art section :D

--- scripts/adblockNotice.js
+++ scripts/adblockNotice.js
@@ -2,21 +2,21 @@
 // https://stefanbohacek.com/project/detect-missing-adblocker-wordpress-plugin/
 
 function shouldDisplayNotice() {
-  return localStorage.getItem('noticeClosed') !== 'true';
+  return localStorage.getItem("noticeClosed") !== "true";
 }
 
 if (shouldDisplayNotice()) {
-  document.getElementById('ftf-dma-note').style.display = 'block';
+  document.getElementById("ftf-dma-note").style.display = "block";
 } else {
-  document.getElementById('ftf-dma-note').style.display = 'none';
+  document.getElementById("ftf-dma-note").style.display = "none";
 }
 
 // close notice and store
 function closeNotice() {
-  var notice = document.getElementById('ftf-dma-note');
-  notice.style.animation = 'slide-down 0.5s ease-out'; // slides out of dms
+  var notice = document.getElementById("ftf-dma-note");
+  notice.style.animation = "slide-down 0.5s ease-out"; // slides out of dms
   setTimeout(function () {
-    notice.style.display = 'none';
-    localStorage.setItem('noticeClosed', 'true'); // store it
+    notice.style.display = "none";
+    localStorage.setItem("noticeClosed", "true"); // store it
   }, 500);
 }
---
+++ scripts/artDisplay.js
@@ -0,0 +1,34 @@
+let artData;
+let currentArtIndex = 0;
+
+// update art display
+function updateArtDisplay() {
+  const art = artData[currentArtIndex];
+  document.getElementById("art-image").src = art.image;
+  document.getElementById("artist-name").textContent = art.artist;
+  document.getElementById("art-description").textContent = art.description;
+}
+
+// get json file
+async function fetchArtData() {
+  try {
+    const response = await fetch("/json/art.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    artData = await response.json();
+    updateArtDisplay();
+  } catch (error) {
+    console.error("Error fetching art data:", error);
+  }
+}
+
+// listen to button
+document.getElementById("next-button").addEventListener("click", () => {
+  //currentArtIndex = (currentArtIndex + 1) % artData.length;
+  //updateArtDisplay();
+  alert("This is my only piece for now, so there is no other art :(\nBut it still is pretty cool!");
+});
+
+// get art during page load
+fetchArtData();
